956a59317fc370016e8d17ce12029394acb96e3d
optobin <oliviatobin31@gmail.com>
2021-03-10 23:51:19 -0800

    Committing before switching

--- 165a-winter-2021/template/transaction_worker.py
+++ 165a-winter-2021/template/transaction_worker.py
@@ -2,6 +2,7 @@
 from template.table import Table, Record
 from template.index import Index
 from template.planner import Planner
+import threading
 
 class TransactionWorker:
 
@@ -12,6 +13,7 @@ class TransactionWorker:
         self.stats = []
         self.transactions = []
         self.result = 0
+        self.thread = threading.Thread(target=self.start_thread, args=())
         pass
 
     """
@@ -20,12 +22,17 @@ class TransactionWorker:
     def add_transaction(self, t):
         self.transactions.append(t)
 
-    """
-    Runs a transaction
-    """
-    def run(self):
+    # Starts the thread by calling planner.
+    def start_thread(self):
         planner = Planner(self)
         planner.plan()
         self.stats = planner.execute()
         # stores the number of transactions that committed
         self.result = len(list(filter(lambda x: x, self.stats)))
+
+    """
+    Runs a transaction
+    """
+    def run(self):
+        # Calls start_thread --> Planner, execute, etc..
+        self.thread.start()
